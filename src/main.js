fbabd0aab1a3519fc5180a48ae486f009e96a3d4
Gavin Quirk <gavinquirk@gmail.com>
2019-10-30 16:30:40 -0700

    db config, env var, Calendar component created

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueTextAreaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+require('dotenv').config();
+
+Vue.use(VueTextAreaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_KEY,
+  authDomain: 'vuetify-calendar-463ce.firebaseapp.com',
+  databaseURL: 'https://vuetify-calendar-463ce.firebaseio.com',
+  projectId: 'vuetify-calendar-463ce',
+  storageBucket: 'vuetify-calendar-463ce.appspot.com',
+  messagingSenderId: '265532163454',
+  appId: '1:265532163454:web:839813762d77937568df2f',
+  measurementId: 'G-VEMK9J2WYJ'
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
